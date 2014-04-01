721625f7783d58f93f9a06ef8b68e69588ad6778
Xie Zhenye <xiezhenye@gmail.com>
2014-04-01 09:30:37 +0800

    use 'ip addr' instead of 'ifconfig'

--- scripts/harp_init.sh
+++ scripts/harp_init.sh
@@ -10,15 +10,9 @@ fi
 echo 1 >/proc/sys/net/ipv4/conf/all/arp_ignore
 echo 2 >/proc/sys/net/ipv4/conf/all/arp_announce
 
-ifid=`/sbin/ifconfig|gawk -F'[: ]+' 'BEGIN{i=0} $0 ~ "^lo:" { if ($2>i){i=$2} } $4=="'$ip'"&& $2=="inet"{exit 1} END{print i+1}'`
+ip addr add "$1/32" dev lo
 if [ "$?" -ne 0 ]; then
-  echo "ip $ip already exists" >&2
-  exit 2
-fi
-
-/sbin/ifconfig "lo:$ifid" "$1" netmask 255.255.255.255
-if [ "$?" -ne 0 ]; then
-  echo "bind ip $1 to lo:$ifid failed" >&2
+  echo "bind ip $1 to lo failed" >&2
   exit 3
 fi
 
